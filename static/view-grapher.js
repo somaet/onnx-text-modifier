36ab9e7924fa31e5a3138e18cb66c1bb35c4ec93
somaet <etri32666@etri.re.kr>
2023-07-31 17:31:39 +0900

    i is not defined

--- static/view-grapher.js
+++ static/view-grapher.js
@@ -276,7 +276,6 @@ grapher.Node = class {
             this.element.setAttribute('id', this.id);
             //console.log(convertNodeNames([this.id])[0])
         } 
-        i
         this.element.setAttribute('class', this.class ? 'node2 ' + this.class : 'node2');
         // this.element.setAttribute('class', this.class ? 'node ' + this.class : 'node');
         this.element.style.opacity = 0;
